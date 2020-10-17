bc28d9746a8651d31043c38b5607d6ab6f0adae8
Luis Felipe Jimenez <jimluisf@gmail.com>
2020-10-17 18:36:10 -0400

    initial commit

---
+++ src/main/java/com/jimluisf/springbootsecurity/security/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.jimluisf.springbootsecurity.security;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService
+{
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException 
+	{
+		List<SimpleGrantedAuthority> roles = null;
+		
+		if(userName.equals("admin"))
+		{
+			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+			return new User("admin", "$2a$10$ySM1Cqeqt9CDF688Eij9DOQOAH6Ozk3sOFRTLjRX3yQi3.8Vuo.gq", roles);
+		}
+		
+		else if(userName.equals("user"))
+		{
+			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+			return new User("user", "$2a$10$pvA2u2XCkdcF/O3AVM1f1.QJ5/NMGUqEreDEgbL8P1SIUzVwDnmrO", roles);
+		}
+		
+		throw new UsernameNotFoundException("User not found with userName: "+userName);
+		
+	}
+
+}
